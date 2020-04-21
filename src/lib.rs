6bb558f3d8cd1e65173ded999cb8c90b8915d5bd
Haseeb ul Hassan <haseeb.ee12@gmail.com>
2020-04-21 13:10:55 +0500

    Update the library crate by adding the line to print the user's input.

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,11 @@
 pub mod convertor {
     pub fn celsius_to_fahrenheit(temp: f64) {
+        println!("You entered: {}°", temp);
         let temp = (temp * 1.8) + 32.0;
         println!("The temperature in Fahrenheit is: {:.2}°",temp);
 }
     pub fn fahrenheit_to_celsius(temp: f64) {
+        println!("You entered: {}°", temp);
         let temp = (temp - 32.0) / 1.8 ;
         println!("The temperature in Celsius is: {:.2}°",temp);
     }
